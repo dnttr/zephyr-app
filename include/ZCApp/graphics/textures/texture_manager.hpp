69b7210acfa4af2151cc9a33ce57f26a22cb9ea4
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-25 00:25:48 +0200

    update texture_manager to support texture deletion and cleanup; change add_texture to accept GLuint texture id

--- include/ZCApp/graphics/textures/texture_manager.hpp
+++ include/ZCApp/graphics/textures/texture_manager.hpp
@@ -16,11 +16,39 @@ namespace zc_app
         std::unordered_map<std::string, GLuint> textures;
 
     public:
-        void add_texture(const std::string &name, const int width, const int height)
+        void add_texture(const std::string &name, GLuint texture)
         {
             if (textures.contains(name))
             {
                 throw std::runtime_error("Texture with name '" + name + "' already exists.");
+            }
+
+            textures.emplace(name, texture);
+        }
+
+        void delete_texture(const std::string &name)
+        {
+            if (const auto it = textures.find(name); it != textures.end())
+            {
+                const unsigned texture_id = it->second;
+
+                if (texture_id != 0)
+                {
+                    glDeleteTextures(1, &texture_id);
+                    textures.erase(it);
+                }
+            }
+        }
+
+        void cleanup()
+        {
+            for (const auto& [name, texture] : textures)
+            {
+                if (texture != 0)
+                {
+                    glDeleteTextures(1, &texture);
+                    textures.erase(name);
+                }
             }
         }
     };
